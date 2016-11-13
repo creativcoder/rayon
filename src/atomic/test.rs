fa780ea42ce732b9ca6c9e4de8c2bb8aee668b5c
Niko Matsakis <niko@alum.mit.edu>
2016-11-13 11:25:32 -0500

    add `atomic` module
    Implement a flat combining lock. Falls back to mutex when not using
    Rayon.

---
+++ src/atomic/test.rs
@@ -0,0 +1,83 @@
+use Configuration;
+use scope;
+use ThreadPool;
+use super::Atomic;
+
+use std::sync::Arc;
+use std::thread;
+
+const THREADS: usize = 100;
+
+#[test]
+fn random_threads() {
+    let mut counter = 0;
+    let atomic = Arc::new(Atomic::new(move |data: usize| -> usize {
+        let value = counter;
+        counter += data;
+        value
+    }));
+
+    let handles: Vec<_> =
+        (0..THREADS).map(|i| {
+                        let atomic = atomic.clone();
+                        thread::spawn(move || {
+                            atomic.invoke(i);
+                        })
+                    })
+                    .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let sum = atomic.invoke(0);
+    assert_eq!(sum, (0..THREADS).sum());
+}
+
+#[test]
+fn random_threads_scope() {
+    let mut counter = 0;
+
+    {
+        let atomic = &Atomic::new(|data| counter += data);
+        invoke_atomic(atomic);
+    }
+
+    assert_eq!(counter, (0..THREADS).sum());
+}
+
+#[test]
+fn random_threads_scope_multiple_registries() {
+    let mut counter = 0;
+
+    {
+        let atomic = &Atomic::new(|data| counter += data);
+
+        scope(|s| {
+            s.spawn(move |_| {
+                ThreadPool::new(Configuration::new()).unwrap().install(move || {
+                    invoke_atomic(atomic);
+                });
+            });
+
+            s.spawn(move |_| {
+                ThreadPool::new(Configuration::new()).unwrap().install(move || {
+                    invoke_atomic(atomic);
+                });
+            });
+        });
+    }
+
+    let sum: usize = (0..THREADS).sum();
+    assert_eq!(counter, sum * 2);
+}
+
+fn invoke_atomic<F>(atomic: &Atomic<F, usize, ()>)
+    where F: FnMut(usize) -> () + Send
+{
+    scope(|s| {
+        for i in 0..THREADS {
+            s.spawn(move |_| atomic.invoke(i));
+        }
+    });
+}
